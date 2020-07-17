0f1500adf28fdd03a5e78af77ceef72be791bae8
Adowell25 <Adowell25@gmail.com>
2020-07-17 09:40:29 -0500

    initial commit, App setup complete

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+
+//display static content from "public" directory in the application directory
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controllers");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("main server.js listening on: http://localhost:" + PORT);
+});
